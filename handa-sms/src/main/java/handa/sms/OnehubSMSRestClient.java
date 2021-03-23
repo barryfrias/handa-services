64c38ade688280ed9c67f9e4bab778b6c5bf0f43
Barry Richardson L. Frias <blfrias@pldt.com.ph>
2021-03-23 15:14:08 +0800

    forwarding of sms from 7538 to onehub

---
+++ handa-sms/src/main/java/handa/sms/OnehubSMSRestClient.java
@@ -0,0 +1,42 @@
+package handa.sms;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import handa.beans.dto.SmsInbound;
+
+public class OnehubSMSRestClient
+{
+    static Logger log = LoggerFactory.getLogger(OnehubSMSRestClient.class);
+
+    private WebTarget wsTarget;
+
+    public OnehubSMSRestClient(Client jerseyClient, String smsSenderUrl)
+    {
+        this.wsTarget = jerseyClient.target(smsSenderUrl);
+    }
+
+    public void forwardSMS(SmsInbound smsInbound)
+    {
+        checkNotNull(smsInbound, "smsInbound should not be null.");
+        try
+        {
+            Form form = new Form();
+            form.param("from", smsInbound.getMobileNumber());
+            form.param("text", smsInbound.getMessage());
+            // async call
+            wsTarget.request().header("Content-Type", "application/x-www-form-urlencoded").async().post(Entity.form(form)).get().close();
+        }
+        catch(Exception e)
+        {
+            log.error("SMS FORWARDING TO ONEHUB EXCEPTION", e);
+        }
+    }
+}
